f34ea7481fadd06e2ab61d2664ba3357d71fe982
Damirali <kutmanov@protonmail.com>
2026-05-05 18:53:08 +0000

    insertion sort

---
+++ insertion.go
@@ -0,0 +1,16 @@
+package sortalg
+
+// time complexity: O(n^2) worst/average, O(n) best
+// space complexity: O(1)
+
+func InsertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
